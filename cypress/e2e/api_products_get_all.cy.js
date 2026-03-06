6d148cc622cef769ee9065acad62229dbe69b35d
Eduardo0015 <eduardohsfreitas86@gmail.com>
2026-03-06 10:06:52 -0300

    Adicionando testes automatizados de API para endpoints de produtos

---
+++ cypress/e2e/api_products_get_all.cy.js
@@ -0,0 +1,20 @@
+it('Should get all products', () => {
+
+  const product = {
+    title: "Structure Test Product",
+    price: 30,
+    description: "Testing response structure",
+    image: "https://i.pravatar.cc",
+    category: "electronics"
+  }
+  
+  cy.request({
+    method: 'GET',
+    url: 'https://fakestoreapi.com/products',
+    body: product
+  }).then((response) => {
+    expect(response.status).to.be.eq(200)
+    expect(response.body).to.be.an('array')
+    expect(response.body.length).to.be.greaterThan(0)
+  })
+})
